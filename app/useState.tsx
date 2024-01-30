b4f6935c3c0d8cdb86727fc3ddde4db3ea36b2ec
csm <csm009177@gmail.com>
2024-01-30 23:36:50 +0900

    about useState

--- app/useState.tsx
+++ app/useState.tsx
@@ -4,11 +4,16 @@ import React, { useState } from "react";
 
 export default function UseState() {
   const [count, setCount] = useState(0);
-  
+
+  const handleUpdate = () => {
+    setCount(count+1)
+  }
+  console.log('update')
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <div>클릭 횟수 </div>
+      <div>You clicked {count} times</div>
+      <button onClick={handleUpdate}>click</button>
     </div>
   );
 }
