cb04cd08ed0c9d4c814c644732d190da6bbab7d6
csm <csm009177@gmail.com>
2024-01-31 12:09:00 +0900

    useRef 컴포넌트 생성 (미완)

---
+++ app/useRef.tsx
@@ -0,0 +1,25 @@
+
+'use client'
+import React, { useState } from "react";
+
+export default function useRef() {
+  const [count, setCount] = useState(1);
+
+  const handleUpdate = () => {
+    let newCount;
+    if(count>=12){
+      newCount=1;
+    } else {
+      newCount = count +1
+    }
+    setCount(newCount)
+  }
+  console.log('update')
+  return (
+    <div>
+      <div>클릭 횟수 </div>
+      <div>You clicked {count} times</div>
+      <button onClick={handleUpdate}>click</button>
+    </div>
+  );
+}
