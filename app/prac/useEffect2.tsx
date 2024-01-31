f40010e5dae2fe4ca67b70ee4cd107695793b398
csm <csm009177@gmail.com>
2024-01-31 11:45:16 +0900

    timer 끝내는 것

--- app/prac/useEffect2.tsx
+++ app/prac/useEffect2.tsx
@@ -1,6 +1,5 @@
 
 'use client'
-
 import React, { useState, useEffect } from 'react';
 
 export default function UseEffect() {
@@ -9,19 +8,13 @@ export default function UseEffect() {
   
 
   useEffect(() => {
-    let timer;
-    if (show) {
-      timer = setInterval(() => {
-        setCount(prevCount => prevCount + 1);
-        console.log("running");
-      }, 1000);
-    } else {
-      clearInterval(timer);
-    }
+    const timer = setInterval(() => {
+      setCount(prevCount => prevCount + 1);
+    }, 1000);
 
     // Clean-up 함수를 반환하여 컴포넌트가 언마운트될 때 setInterval을 정리합니다.
     return () => clearInterval(timer);
-  }, [show]); // show 상태를 의존성 배열에 추가하여 show 상태가 변경될 때마다 useEffect가 실행되도록 합니다.
+  }, []); // 빈 배열을 전달하여 처음 한 번만 실행되도록 합니다.
 
   return (
     <div>
