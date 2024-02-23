c8e79b64bebadf3b4dee98c99f3f422d9d9e57b8
csm <csm009177@gmail.com>
2024-02-24 00:27:01 +0900

    import state from react

--- app/useRef.tsx
+++ app/useRef.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import React, { useState, useRef } from "react";
+
 const UseRef = () => {
   const [count, setCount] = useState(1);
-
+  const ref = useRef()
   return (
     <div>
       {/* useRef로 생성된 ref를 input 요소에 연결합니다. */}
